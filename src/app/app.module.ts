dacf29aa79a7810087ead4d0f4303a0c3503275c
Hans Buker <hansbuker1993@gmail.com>
2023-05-24 17:46:05 -0400

    url checker dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UrlTesterComponent } from './url-tester/url-tester.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UrlTesterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
